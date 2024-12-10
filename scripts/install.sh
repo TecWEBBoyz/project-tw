0a0c69757907b274f7822e1b4c9228942fe83d02
Davide Baraldo <baraldodavide@gmail.com>
2024-12-10 17:47:03 +0100

    [EVO] Debugging config

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 find . ! -name "install.sh" -exec rm -rf {} +
 
 # Download latest repo change
- curl -L -o repo.zip https://github.com/TecWEBBoyz/project-tw/archive/refs/heads/deploy.zip
- unzip repo.zip
+curl -L -o repo.zip https://github.com/TecWEBBoyz/project-tw/archive/refs/heads/deploy.zip
+unzip repo.zip
 
 # Manage file position
 mv ./project-tw-deploy/* .
--- setup_apache.sh
+++ setup_apache.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Aggiornamento dei pacchetti e installazione dei necessari
-apt-get update && apt-get install -y apache2 php8.1 libapache2-mod-php8.1 mariadb-server=1:10.6.7* php8.1-xdebug && apt-get clean
+apt-get update && apt-get install -y \
+    apache2 \
+    php8.1 \
+    libapache2-mod-php8.1 \
+    mariadb-server=1:10.6.7* \
+    php8.1-xdebug \
+    unzip \
+    curl \
+    && apt-get clean
 
 # Configurazione di Apache
 echo 'ServerName localhost' >> /etc/apache2/apache2.conf
@@ -21,11 +29,22 @@ xdebug.client_host=host.docker.internal
 xdebug.client_port=9003
 xdebug.log=/var/log/xdebug.log
 xdebug.remote_autostart=1
-xdebug.idekey=docker
+xdebug.idekey=PHPSTORM
 EOL
 
 # Abilita Xdebug per PHP
 phpenmod xdebug
+
+# Modifica del file php.ini
+PHP_INI_FILE="/etc/php/8.1/apache2/php.ini"
+
+sed -i 's/display_errors = .*/display_errors = On/' $PHP_INI_FILE
+sed -i 's/log_errors = .*/log_errors = On/' $PHP_INI_FILE
+sed -i 's/error_reporting = .*/error_reporting = E_ALL/' $PHP_INI_FILE
+
+# Installazione di Composer
+curl -sS https://getcomposer.org/installer | php -- \
+--install-dir=/usr/bin --filename=composer
 
 # Riavvio dei servizi per applicare le configurazioni
 service apache2 restart
