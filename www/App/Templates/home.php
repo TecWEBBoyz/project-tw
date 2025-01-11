690200f7f0b80e5bf303c6d9de19342d2fa6003e
Davide Baraldo <baraldodavide@gmail.com>
2025-01-11 13:42:40 +0100

    (feat) home.php

--- www/App/Templates/home.php
+++ www/App/Templates/home.php
@@ -28,9 +28,13 @@ use function PTW\config;
 
         $image = $image->ToArray();
 
-        $detailPage = "gallerydetails?src=" . urlencode($image[ImageType::path->value]) . "&description=" . urlencode($image[ImageType::description->value]). "&location=" . urlencode($image[ImageType::place->value]) . "&alt=" . urlencode($image[ImageType::alt->value]);
+        $detailPage = "gallerydetails?src=" . urlencode($image[ImageType::path->value]) . "&description=" . urlencode($image[ImageType::description->value]) . "&location=" . urlencode($image[ImageType::place->value]) . "&alt=" . urlencode($image[ImageType::alt->value]) . "&date=" . urlencode($image[ImageType::date->value]);
         #replace image extension with _25percent.jpg manage upper and lower case extensions
         $imagePathResized = preg_replace('/\.(jpg|jpeg|png)$/i', '_25percent.jpg', $image[ImageType::path->value]);
+
+        # Add date handling
+        $date = isset($image[ImageType::date->value]) ? htmlspecialchars($image[ImageType::date->value], ENT_QUOTES, 'UTF-8') : 'Unknown date';
+
         echo "<div class='gallery-item' data-description='" . htmlspecialchars($image[ImageType::description->value], ENT_QUOTES, 'UTF-8') . "'>
         <a href='" . htmlspecialchars($detailPage, ENT_QUOTES, 'UTF-8') . "'>
             <div class='image-wrapper'>
@@ -38,8 +42,9 @@ use function PTW\config;
             </div>
         </a>
         <div class='info'>
-            <span>" . htmlspecialchars($image[ImageType::alt->value], ENT_QUOTES, 'UTF-8') . "</span>
+            <span>" . htmlspecialchars($image[ImageType::title->value], ENT_QUOTES, 'UTF-8') . "</span>
             <span>Location: " . htmlspecialchars($image[ImageType::place->value], ENT_QUOTES, 'UTF-8') . "</span>
+            <span>Date: " . $date . "</span>
         </div>
     </div>";
     }
