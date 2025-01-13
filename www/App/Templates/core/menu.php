91517331dfc67c0b386a0c58e695cb6c46924b24
Leo <leonardo.trolese@studenti.unipd.it>
2025-01-13 14:22:04 +0100

    (feat) aggiunti aiuti alla navigazione, vanno nascosti

--- www/App/Templates/core/menu.php
+++ www/App/Templates/core/menu.php
@@ -4,34 +4,34 @@ use PTW\Modules\Auth\Role;
 use PTW\Modules\Auth\SessionManager;
 
 $menuItems = [
-    //['label' => 'Vai al contenuto', 'link' => '#content', 'ariaLabel' => 'Collegamento al contenuto'],
-    ['label' => 'Home', 'link' => 'home', 'ariaLabel' => 'Collegamento alla pagina Home'],
-    ['label' => 'About', 'link' => 'about', 'ariaLabel' => 'Collegamento alla pagina About'],
-    ['label' => 'Services', 'link' => 'services', 'ariaLabel' => 'Collegamento alla pagina Services'],
-    ['label' => 'Contact', 'link' => 'contact', 'ariaLabel' => 'Collegamento alla pagina Contact'],
-    //['label' => 'Vai al footer', 'link' => '#footer', 'ariaLabel' => 'Collegamento al piè di pagina'],
+    ['label' => 'Vai al contenuto', 'link' => '#content', 'ariaLabel' => 'Collegamento al contenuto', 'classes' => ' navigation-help'],
+    ['label' => 'Vai al footer', 'link' => '#footer', 'ariaLabel' => 'Collegamento al piè di pagina', 'classes' => ' navigation-help'],
+    ['label' => 'Home', 'link' => 'home', 'ariaLabel' => 'Collegamento alla pagina Home', 'classes' => ''],
+    ['label' => 'About', 'link' => 'about', 'ariaLabel' => 'Collegamento alla pagina About', 'classes' => ''],
+    ['label' => 'Services', 'link' => 'services', 'ariaLabel' => 'Collegamento alla pagina Services', 'classes' => ''],
+    ['label' => 'Contact', 'link' => 'contact', 'ariaLabel' => 'Collegamento alla pagina Contact', 'classes' => ''],
 ];
 
 $sessionManager = new SessionManager();
 if ($sessionManager->isAuthenticated()) {
     if ($sessionManager->authorize(Role::User)) {
-        $menuItems[] = ['label' => 'Dashboard', 'link' => 'dashboard', 'ariaLabel' => ' Collegamento alla pagina Dashboard'];
+        $menuItems[] = ['label' => 'Dashboard', 'link' => 'dashboard', 'ariaLabel' => ' Collegamento alla pagina Dashboard', 'classes' => ''];
     }
     if($sessionManager->authorize(Role::Administrator)) {
-        $menuItems[] = ['label' => 'Admin', 'link' => 'admin', 'ariaLabel' => 'Collegamento alla pagina Admin'];
+        $menuItems[] = ['label' => 'Admin', 'link' => 'admin', 'ariaLabel' => 'Collegamento alla pagina Admin', 'classes' => ''];
     } else {
-        $menuItems[] = ['label' => 'Profile', 'link' => 'profile', 'ariaLabel' => 'Collegamento alla pagina Profile'];
-        $menuItems[] = ['label' => 'Book Service', 'link' => 'book-service', 'ariaLabel' => 'Book service page link'];
+        $menuItems[] = ['label' => 'Profile', 'link' => 'profile', 'ariaLabel' => 'Collegamento alla pagina Profile', 'classes' => ''];
+        $menuItems[] = ['label' => 'Book Service', 'link' => 'book-service', 'ariaLabel' => 'Book service page link', 'classes' => ''];
     }
-    $menuItems[] = ['label' => 'Logout', 'link' => 'logout', 'ariaLabel' => 'Effetua Logout'];
+    $menuItems[] = ['label' => 'Logout', 'link' => 'logout', 'ariaLabel' => 'Effetua Logout', 'classes' => ''];
 } else {
-    $menuItems[] = ['label' => 'Login', 'link' => 'login', 'ariaLabel' => 'Collegamento alla pagina Login'];
+    $menuItems[] = ['label' => 'Login', 'link' => 'login', 'ariaLabel' => 'Collegamento alla pagina Login', 'classes' => ''];
 //    $menuItems[] = ['label' => 'Register', 'link' => 'register', 'ariaLabel' => 'Register page link'];
 }
 function renderMenu($menuItems, $mobile = false) {
     echo '<nav id="menu"><ul><span lang="eng">';
     foreach ($menuItems as $item) {
-        echo '<li><a class="nav-link"'.($mobile ? ' data-mobile="true" ' : ' ' ).'href="' . htmlspecialchars($item['link']) . '" aria-label="' . htmlspecialchars($item['ariaLabel']) . '">' . htmlspecialchars($item['label']) . '</a></li>';
+        echo '<li><a class="' . htmlspecialchars($item['classes']) . '"'.($mobile ? ' data-mobile="true" ' : ' ' ).'href="' . htmlspecialchars($item['link']) . '" aria-label="' . htmlspecialchars($item['ariaLabel']) . '">' . htmlspecialchars($item['label']) . '</a></li>' . PHP_EOL;
     }
     echo '</span></ul></nav>';
 }
@@ -42,7 +42,7 @@ function renderMenu($menuItems, $mobile = false) {
     <div class="links">
         <?php renderMenu($menuItems); ?>
     </div>
-    <button class="hamburger" role="button" aria-label="Apertura menu di navigazione" tabindex="0" onclick="toggleMenu()">
+    <button class="hamburger" aria-label="Apertura menu di navigazione" tabindex="0" onclick="toggleMenu()">
         <div></div>
         <div></div>
         <div></div>
