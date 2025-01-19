e785175d9443147cd5c3e4e91a910cfadcc449f6
Leo <leonardo.trolese@studenti.unipd.it>
2025-01-19 17:06:22 +0100

    (feat) started about page

--- www/App/Templates/about.php
+++ www/App/Templates/about.php
@@ -1,1 +1,14 @@
-ABOUT PAGE
+<div id="about-page">
+    <!--<img src="../../static/images/me.jpg"> -->
+    <div id="temp-me-image"></div>
+    <h1>Filippo Rizzato</h1>
+    <p>
+    Benvenuti nel mio mondo, dove fotografia significa catturare il crudo ordinario e trasformarlo in istanti 
+    straordinari e irripetibili, rivelando così l'essenza dell'arte. Non esiste un giusto o uno sbagliato, 
+    ma solo l'istinto del momento che plasmerà il nostro futuro.
+    </p>
+    <p>
+    In un'esistenza sempre più dinamica e frenetica, la fotografia ci permette, e ci impone, di rallentare e 
+    di ascoltarci in maniera introspettiva, per cogliere la vera essenza di noi stessi.
+    </p>
+</div>
