112f74b7f034073e5a87546fa819b934e4dbd779
Leo <leonardo.trolese@studenti.unipd.it>
2025-01-19 18:16:38 +0100

    (feat) draft about page

--- www/App/Templates/about.php
+++ www/App/Templates/about.php
@@ -1,7 +1,10 @@
 <div id="about-page">
-    <!--<img src="../../static/images/me.jpg"> -->
-    <div id="temp-me-image"></div>
-    <h1>Filippo Rizzato</h1>
+    <img src="static/images/me.jpg" id="filippo-image">
+    <div>
+    <h2><span lang="en">About me</span></h2>
+    <hr>
+    <h3>Filippo Rizzato</h3>
+    <hr>
     <p>
     Benvenuti nel mio mondo, dove fotografia significa catturare il crudo ordinario e trasformarlo in istanti 
     straordinari e irripetibili, rivelando così l'essenza dell'arte. Non esiste un giusto o uno sbagliato, 
@@ -11,4 +14,5 @@
     In un'esistenza sempre più dinamica e frenetica, la fotografia ci permette, e ci impone, di rallentare e 
     di ascoltarci in maniera introspettiva, per cogliere la vera essenza di noi stessi.
     </p>
+    </div>
 </div>
---
+++ www/Config/config.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Basic template configuration file.
+ * 
+ * Create a new config.php file in the same directory and copy the contents of this file into it.
+ * Replace the values as needed.
+ */
+
+/**
+ * Template Configuration Array
+ */
+return [
+    "router" => [
+        "baseURL" => "/username",    // Base URL for the application (Nome utente su server, / in locale)
+        "app" => [
+            "debug" => true
+        ]
+    ],
+    "database" => [
+        "host" => "localhost",
+        "username" => "default_user",
+        "password" => "default_password",
+        "database" => "default_database",
+    ]
+];
