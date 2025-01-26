ece96da5c6c63cc934912bff05709f288061f250
LucaMonetti <monettiluca03@gmail.com>
2025-01-26 16:37:44 +0100

    [refactor] Refactor code of table

--- www/App/Templates/profile.php
+++ www/App/Templates/profile.php
@@ -9,54 +9,68 @@ if (!isset($TEMPLATE_DATA)) {
 
 ?>
 
-<h2>Accesso effettuato come: <?php echo $TEMPLATE_DATA["user"]["username"] ?></h2>
+<h2>Welcome <?php echo $TEMPLATE_DATA["user"]["username"] ?></h2>
 
-<h2>Appuntamenti Fissati</h2>
+<article>
+    <header>
+        <p class="caption">/services</p>
+        <h3>Booked Service</h3>
+    </header>
 
+    <?php if (isset($TEMPLATE_DATA["bookings"]) && count($TEMPLATE_DATA["bookings"]) > 0) : ?>
 
-<?php
-
-if (isset($TEMPLATE_DATA["bookings"]) && count($TEMPLATE_DATA["bookings"]) > 0) {
-    ?>
-    <div class="table-responsive">
-    <table>
-        <thead>
-            <tr>
-                <th>Servizio</th>
-                <th>Data</th>
-                <th>Stato</th>
-                <th>Azioni</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            /** @var Booking $booking */
-            foreach ($TEMPLATE_DATA["bookings"] as $booking) {
-                ?>
-
+        <div class="table-responsive">
+            <table>
+                <thead>
                 <tr>
-                <td><?php echo $booking->ToArray()[\PTW\Models\BookingType::service->value] ?></td>
-                <td><?php echo $booking->ToArray()[\PTW\Models\BookingType::date->value] ?></td>
-                <td><?php echo $booking->ToArray()[\PTW\Models\BookingType::status->value] ?></td>
-                <td>
-                <a href="delete">Delete</a>
-                </td>
+                    <th>ID</th>
+                    <th>Service</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th>Actions</th>
                 </tr>
-
+                </thead>
+                <tbody>
                 <?php
-            }
-            ?>
-        </tbody>
-    </table>
-    </div>
-    <?php
-} else {
-    ?>
+                /** @var Booking $booking */
+                foreach ($TEMPLATE_DATA["bookings"] as $index => $booking) {
+                    ?>
 
-    <p>Non è stato ancora fissato un appuntamento:</p>
-    <a href="book-service">Prenota ora un appuntamento</a>
+                    <tr>
+                        <td data-label="id"><?php echo $index + 1; ?></td>
+                        <td data-label="service"><?php echo $booking->ToArray()[\PTW\Models\BookingType::service->value] ?></td>
+                        <td data-label="date"><?php echo $booking->ToArray()[\PTW\Models\BookingType::date->value] ?></td>
+                        <td data-label="status" class="status cancelled"><span><?php echo $booking->ToArray()[\PTW\Models\BookingType::status->value] ?></span></td>
+                        <td data-label="actions" class="actions">
+                            <ul>
+                                <li>
+                                    <button class="icon-button icon-button-primary">
+                                        <?php echo file_get_contents("static/images/edit.svg"); ?>
+                                        <span>Edit</span>
+                                    </button>
+                                </li>
+                                <li>
+                                    <button class="icon-button icon-button-danger">
+                                        <?php echo file_get_contents("static/images/delete.svg"); ?>
+                                        <span>Delete</span>
+                                    </button>
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
 
-    <?php
-}
+                    <?php
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+    <?php else: ?>
 
-?>
+        <p>Non è stato ancora fissato un appuntamento:</p>
+        <a href="book-service">Prenota ora un appuntamento</a>
+
+    <?php endif; ?>
+
+
+</article>
