3aa4a0b2aab5b5b57bc4838d0e28924f0a498764
Davide Baraldo <baraldodavide@gmail.com>
2025-01-31 11:40:51 +0100

    (fix) skill issue

--- www/App/Templates/404.php
+++ www/App/Templates/404.php
@@ -3,7 +3,7 @@
         <h1>404</h1>
         <p><?php echo \PTW\translationWithSpan('not-found-title'); ?></p>
         <p><?php echo \PTW\translationWithSpan('not-found-text'); ?></p>
-        <a href="home"><?php echo \PTW\translation("go-homepage"); ?></a>
+        <a href="home" class="link"><?php echo \PTW\translation("go-homepage"); ?></a>
     </div>
     <div class="image-500"></div>
 </div>
--- www/App/Templates/500.php
+++ www/App/Templates/500.php
@@ -3,7 +3,7 @@
         <h1>500</h1>
         <p class="not-found-error"><?php if(isset($TEMPLATE_DATA['errorstring']) and $TEMPLATE_DATA['errorstring'] != "") echo $TEMPLATE_DATA['errorstring']; else echo \PTW\translationWithSpan("system-error-title"); ?></p>
         <p><?php echo \PTW\translationWithSpan("system-error-text"); ?></p>
-        <a href="home"><?php echo \PTW\translation("go-homepage"); ?></a>
+        <a href="home" class="link"><?php echo \PTW\translation("go-homepage"); ?></a>
     </div>
     <div class="image-500"></div>
 </div>
