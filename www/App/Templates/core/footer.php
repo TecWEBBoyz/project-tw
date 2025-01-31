a291d162750e7d06af3d1e8142662b0b2e6bec11
Leo <leonardo.trolese@studenti.unipd.it>
2025-01-31 14:28:49 +0100

    (fix) translationWithSpan manu, home, footer

--- www/App/Templates/core/footer.php
+++ www/App/Templates/core/footer.php
@@ -3,5 +3,5 @@
         <li><a href="https://www.instagram.com/filippor_photography/" aria-label="<?php echo \PTW\translation('footer-instagram-link'); ?>" class="social-link"><?php echo file_get_contents("static/images/ig.svg") ?></a></li>
         <li><a href="https://www.facebook.com/filippo.rizzato.716?locale=it_IT" aria-label="<?php echo \PTW\translation('footer-facebook-link'); ?>" class="social-link"><?php echo file_get_contents("static/images/fb.svg") ?></a></li>
     </ul>
-    <p class="copyright"><?php echo \PTW\translationWithSpan('footer-copyright'); ?></p>
+    <p class="copyright"><?php echo \PTW\translation('footer-copyright'); ?></p>
 </footer>
